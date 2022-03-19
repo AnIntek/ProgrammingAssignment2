ee05ec73dbc43ebc240511cff4587766dee4a25c
AnIntek <anintek@gmail.com>
2022-03-19 22:00:25 +0200

    First not working version with $ $ operator is invalid for atomic vectors

--- cachematrix.R
+++ cachematrix.R
@@ -26,6 +26,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## If the inverse has already been calculated (and the matrix has not changed), 
 ## then the cachesolve should retrieve the inverse from the cache.
 
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m_inverse <- x$getinverse()
